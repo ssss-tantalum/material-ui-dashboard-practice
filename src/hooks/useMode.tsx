f7e6b97efc1ec77aa2e111836b19311f044b89df
ssss.tantalum <ssss.tantalum@gmail.com>
2023-03-01 22:44:31 +0900

    first commit

---
+++ src/hooks/useMode.tsx
@@ -0,0 +1,26 @@
+import { PaletteMode, createTheme } from '@mui/material';
+import { createContext, useState, useMemo } from 'react';
+import { themeSettings } from '../theme';
+
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {}
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState<PaletteMode>('dark');
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === 'light' ? 'dark' : 'light'))
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return {
+    theme,
+    colorMode
+  };
+};
